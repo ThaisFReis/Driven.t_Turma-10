0348b310f0da4354a65f05976762e478044237bf
ThaisFReis <reis.thaisf@gmail.com>
2023-07-03 17:54:07 -0300

    Comentarios adicionados

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -7,8 +7,10 @@ import { exclude } from '@/utils/prisma-utils';
 import { ViaCEPAddress, AddressEnrollment } from '@/protocols';
 import { getAddress } from '@/utils/cep';
 
-// TODO - Receber o CEP por parâmetro nesta função.
+// Receber o CEP por parâmetro nesta função.
 async function getAddressFromCEP(cep: string) {
+
+  // Se eu usar o getAddress, ele não funciona no teste, pois ele não consegue fazer a requisição.
   const address = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
 
   // Verificar se o CEP é válido.
@@ -56,16 +58,18 @@ async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollm
   const address = getAddressForUpsert(params.address);
 
   try{
+    // Receber o CEP por parâmetro nesta função.
     await getAddressFromCEP(address.cep);
   }
 
   catch(error) {
-    throw invalidDataError(['CEP inválido']);
+    // Verificar se o erro é de CEP inválido.
+    throw invalidDataError(['CEP inválido']); // Precisa ser um array pois a função espera um array de strings.
   }
 
   const enrollmentWithAddress = await enrollmentRepository.upsert(params.userId, enrollment, exclude(enrollment, 'userId'));
 
-  await addressRepository.upsert(enrollmentWithAddress.id, address, address);
+  await addressRepository.upsert(enrollmentWithAddress.id, address, address); // Precisa ser passado o endereço duas vezes pois o upsert espera três parâmetros. E precisa ser o adress por causa do CreateAddressParams.
 }
 
 function getAddressForUpsert(address: CreateAddressParams) {
